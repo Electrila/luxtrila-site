0ca667e706666d8e6ec200f1d9a0ad525b91da2f
Megann Comstock <48378396+Electrila@users.noreply.github.com>
2025-04-20 04:22:36 -0400

    Removed nav bar until more testing can be done for mobile

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 particlesJS.load("particles-js", "assets/particles.json", function() {
     console.log("Particles loaded!");
   });
-
-const hamburger = document.getElementById("hamburger");
-const navMenu = document.getElementById("navMenu");
-
-hamburger.addEventListener("click", () => {
-  navMenu.classList.toggle("show");
-});
