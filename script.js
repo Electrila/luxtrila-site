b2eb85eb46e4ff28fc6c394f3451ca1933eb8bd4
Megann Comstock <48378396+Electrila@users.noreply.github.com>
2025-04-20 03:58:08 -0400

    Added simple navbar for placeholder, will refine later

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 particlesJS.load("particles-js", "assets/particles.json", function() {
     console.log("Particles loaded!");
   });
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
